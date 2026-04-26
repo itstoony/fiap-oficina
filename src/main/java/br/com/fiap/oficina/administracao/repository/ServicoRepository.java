59e1d0945321d4e0330267633337241903dd43ee
itstoony <toonyrenner@gmail.com>
2026-04-25 23:43:26 -0300

    feat: implementa bounded context de administracao com catalogo de servicos e relatorio de tempo medio

--- src/main/java/br/com/fiap/oficina/administracao/repository/ServicoRepository.java
+++ src/main/java/br/com/fiap/oficina/administracao/repository/ServicoRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ServicoRepository extends JpaRepository<Servico, UUID> {
+    boolean existsByNome(String nome);
 }
